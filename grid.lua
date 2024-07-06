2a7499e4862eadaacfcbe44b347f5fe085f15500
Zéphyr <antoine.familleroger@gmail.com>
2024-07-06 22:00:12 +0200

    Added towers placement

--- grid.lua
+++ grid.lua
@@ -10,14 +10,11 @@ local map = {}
 local tower_img = love.graphics.newImage("tower.png")
 
 
-
-
 function grid.load()
   local file = 'map1.txt'
   local lines = utils.lines_from(file)
 
   for i,line in pairs(lines) do
-    -- print('line[' .. k .. ']', v)
     local temp = {}
     for j = 1, #line do
         local c = line:sub(j, j)
@@ -26,13 +23,6 @@ function grid.load()
     map[i] = temp
   end
 
-
-  -- for j=1, Height do
-  --     for i=1, Width do
-  --         print(map[j][i])
-  --     end
-  -- end
-
   local road_img = love.graphics.newImage("road.png")
   local r1 = ui.container:new(300, 100, 50, 50, road_img, "")
   table.insert(ui_grid, r1)
@@ -40,9 +30,13 @@ end
 
 function grid.update()
   if love.mouse.isDown(2) then
-      local x = math.ceil((love.mouse.getX() / WindowWidth) * Width)
-      local y = math.ceil((love.mouse.getY() / WindowHeight) * Height)
-      map[x][y] = 1
+      local offset_x = WindowWidth - 900
+      local offset_y = WindowHeight - 600
+      local x = math.ceil(((love.mouse.getX() - offset_x) / 900) * Width)
+      local y = math.ceil(((love.mouse.getY() - offset_y) / 600) * Height)
+      if y > 0 and x > 0 and map[y][x] == '0' then
+        map[y][x] = '4'
+      end
   end
 end
 
@@ -54,7 +48,7 @@ function grid.draw()
 
   for y=1, Height do
     for x=1, Width do
-      if map[y][x] == '0' then
+      if map[y][x] == '4' then
         local offset_x = WindowWidth - 900
         local offset_y = WindowHeight - 600
         love.graphics.draw(tower_img, offset_x + ((x-1) * 900 / Width), offset_y + ((y-1) * 600 / Height), 0, 0.5, 0.5)
